898f85b0f0bd45012a9839806035ca3a1e7e8291
LUAN GODOY FAUTH <4255795+gfauth@users.noreply.github.com>
2018-03-01 13:30:02 -0300

    Finalização do desenvolvimento da API de calculo de proximidades geograficas.

---
+++ server.js
@@ -0,0 +1,82 @@
+//#region Declaração das variaveis do server
+var express = require("express");
+var bodyParser = require("body-parser");
+var geolib = require("geolib");
+var porta = 8080;
+//#endregion
+
+//#region Propriedades de retorno.
+var sucesso = {
+    status: {
+        codigo: 200,
+        mensagem: "sucesso"
+    },
+    resultado: []
+};
+
+var falha = {
+    status: {
+        codigo: 550,
+        mensagem: "erro"
+    }
+}
+//#endregion
+
+//#region Inicialização do servidor e declaração de midlewares
+var app = express();
+app.use(bodyParser.json());
+//#endregion
+
+//#region funções de GET
+app.get("/", function (req, res) {
+    res.send("Utilize o método POST contendo um objeto Json.");
+});
+
+//#endregion
+
+//#region Funções de POST
+app.post("/", function (req, res) {
+    var retorno;
+    var resultado = [];
+    try {
+        req.body.destinos.forEach(element => {
+            var distancia = geolib.getDistance(
+                {
+                    latitude: req.body.pontoOrigem.lat,
+                    longitude: req.body.pontoOrigem.lon
+                },
+                {
+                    latitude: element.lat,
+                    longitude: element.lon
+                });
+                
+            resultado.push(
+                {
+                    distancia: distancia,
+                    coordenadas: element
+                });
+        });
+
+        resultado = resultado.sort(ordenar);
+        sucesso.resultado = resultado;
+        retorno = sucesso;
+    }
+    catch (ex) {
+        retorno = falha;
+        retorno.erro = ex.message;
+    }
+
+    res.send(retorno);
+});
+
+//#endregion
+
+//Inicialização da escuta do server.
+app.listen(porta, function () {
+    console.log("Servidor está rodando na porta " + porta + ".");
+});
+
+//Função de ordenação usada no sort para obter maior desempenho na reordenação do array.
+function ordenar(a, b) {
+    return (a.distancia - b.distancia);
+};
